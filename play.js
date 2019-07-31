45793f62d1e4ed2609612b6d32aadd38d4b9925c
Jackson Fung <jacksonf21@hotmail.com>
2019-07-31 13:54:28 +0000

    refactored, moved main connect function into client.js

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  conn.setEncoding('utf8');
-
-  //listens to server responses
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  })
-
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log('Connection ...');
 connect();
