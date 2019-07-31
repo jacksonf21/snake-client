c09a00b99c3d2eef73a550a3f5c24a56b3a350b0
Jackson Fung <jacksonf21@hotmail.com>
2019-07-31 16:23:30 +0000

    snake initials

--- client.js
+++ client.js
@@ -6,11 +6,21 @@ const connect = function() {
     port: 50541
   });
   conn.setEncoding('utf8');
+  
+  //on successful connection
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+
+    //send name credentials
+    conn.write('Name: JKF');
+  })
 
   //listens to server responses
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   })
+
+
 
   return conn;
 };
